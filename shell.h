e43e66cd2a1c37b09e0bc129fc13807b51d8a17e
Caesar-12 <idowuenoch65@gmail.com>
2022-11-11 01:14:19 -0800

    Header files

--- shell.h
+++ shell.h
@@ -1,6 +1,12 @@
 #ifndef SHELL_H
 #define SHELL_H
 
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <sys/wait.h>
+#include <sys/type.h>
+#include <fnct1.h>
 
 
 #endif /* #ifndef SHELL_H */
